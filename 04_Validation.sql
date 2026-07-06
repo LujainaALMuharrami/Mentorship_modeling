31702dd7766831cba14a4248ac335d1f77ecd800
LujainaALMuharrami <91949367+LujainaALMuharrami@users.noreply.github.com>
2026-07-06 14:59:37 +0400

    Update 04_Validation.sql

--- 04_Validation.sql
+++ 04_Validation.sql
@@ -1,3 +1,42 @@
-SELECT
-    (SELECT COUNT(*) FROM fact_transactions) AS fact_count,
-    (SELECT COUNT(*) FROM grnrl) AS source_count;
+SELECT 'grnrl (Source Table)' AS table_name, COUNT(*) AS row_count
+FROM grnrl
+
+UNION ALL
+
+SELECT 'dim_date', COUNT(*)
+FROM dim_date
+
+UNION ALL
+
+SELECT 'dim_manufacturer', COUNT(*)
+FROM dim_manufacturer
+
+UNION ALL
+
+SELECT 'dim_payment_type', COUNT(*)
+FROM dim_payment_type
+
+UNION ALL
+
+SELECT 'dim_product', COUNT(*)
+FROM dim_product
+
+UNION ALL
+
+SELECT 'dim_recipient', COUNT(*)
+FROM dim_recipient
+
+UNION ALL
+
+SELECT 'dim_teaching_hospital', COUNT(*)
+FROM dim_teaching_hospital
+
+UNION ALL
+
+SELECT 'dim_travel', COUNT(*)
+FROM dim_travel
+
+UNION ALL
+
+SELECT 'fact_transactions (Fact Table)', COUNT(*)
+FROM fact_transactions;
